40d31875bcf54fbb68903ee9eff22544f399d907
kennyballou <kballou@onyx.boisestate.edu>
2013-06-27 19:36:01 -0600

    Refactor: P3M method

---
+++ nbody/mesh.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import math
+import numpy as np
+
+def get_particles_in_grid_np(bodies, grid):
+    contains = lambda rx, ry, d: d[0] <= rx < d[0] + d[2] and \
+                                 d[1] <= ry < d[1] + d[2]
+    return (x for x in bodies if contains(x[0], x[1], grid))
+
+def get_particles_in_grid(r, grid):
+    contains = lambda r, d: d[0] <= r[0] < d[0] + d[2] and \
+                            d[1] <= r[1] < d[1] + d[2]
+    return (i for i in range(len(r[0])) if contains((r[0][i], r[1][i]), grid))
+
+def create_grids(n, r):
+    '''Create a list (generator) of grids given the count and radius
+
+
+    :param n: number of grids to create
+    :param r: radius of the mesh
+    '''
+    l = 4 * r / n
+    x = list(x for x in range(int(n / 2)))
+    y = list(y for y in range(int(n / 2)))
+    for j in list(y):
+        for i in list(x):
+            yield (-r + l * i, r - l * j, l, l / 2)
--- nbody/simulation.py
+++ nbody/simulation.py
@@ -5,10 +5,18 @@ import sys
 import math
 import numpy as np
 import nbody
+import nbody.mesh as mesh
 
 G = 6.67e-11
 EPS = 3E4
 dt = 0.1
+
+def add_force(rx, ry, fx, fy, m):
+    dx = rx[1] - rx[0]
+    dy = ry[0] - ry[0]
+    dist = math.sqrt(dx ** 2 + dy ** 2)
+    F = (G * m[0] * m[0]) / (dist ** 2 + EPS ** 2)
+    return (fx + dx / dist, fy + dy / dist)
 
 def simulation(rx, ry, vx, vy, fx, fy, m, radius, steps=1):
     '''Do one time step in simulation
@@ -44,24 +52,57 @@ def simulation_step(rx, ry, vx, vy, fx, fy, m, radius):
     :param m:  numpy array of masses
     :param radius: Radius of space
     '''
-    def add_force(i, j):
-        dx = rx[j] - rx[i]
-        dy = ry[j] - ry[i]
-        dist = math.sqrt(dx ** 2 + dy ** 2)
-        F = (G * m[i] * m[j]) / (dist ** 2 + EPS ** 2)
-        fx[i] = fx[i] + dx / dist
-        fy[i] = fy[i] + dy / dist
+    def __add_force(i, j):
+        fx[i], fy[i] = add_force((rx[i], rx[j]),
+                                 (ry[i], ry[j]),
+                                 fx[i], fy[i],
+                                 (m[i], m[j]))
+    def grid_direct(rx, ry, vx, vy, fx, fy, m):
+        fx = np.zeros(len(rx))
+        fy = np.zeros(len(rx))
+        for i in range(len(rx)):
+            for j in range(len(rx)):
+                if i != j:
+                    __add_force(i, j)
+        vx = vx + dt * fx / m
+        vy = vy + dt * fy / m
+        rx = rx + dt * vx
+        ry = ry + dt * vy
+    mesh_64 = mesh.create_grids(16, radius) # returns 64 grids
+    grid_spread = list(list(x) for x in
+                       (mesh.get_particles_in_grid((rx, ry), y)
+                       for y in mesh_64))
+    grids = []
+    for grid in grid_spread:
+        grid_direct(rx[grid],
+                    ry[grid],
+                    vx[grid],
+                    vy[grid],
+                    fx[grid],
+                    fy[grid],
+                    m[grid])
+    for i, grid in enumerate(grid_spread):
+        total_mass = sum(m[grid])
+        if total_mass == 0:
+            continue
+        center_x = sum(m[grid] * rx[grid]) / total_mass
+        center_y = sum(m[grid] * ry[grid]) / total_mass
+        grids.append((center_x, center_y, total_mass, i))
 
-    fx = np.zeros(len(rx))
-    fy = np.zeros(len(rx))
-    for i in range(len(rx)):
-        for j in range(len(rx)):
-            if i != j:
-                add_force(i, j)
-    vx = vx + dt * fx / m
-    vy = vy + dt * fy / m
-    rx = rx + dt * vx
-    ry = ry + dt * vy
+    for a in grids:
+        grid = grid_spread[a[3]]
+        a_fx = 0
+        a_fy = 0
+        for b in grids:
+            if a != b:
+                a_fx, a_fy = add_force((a[0], b[0]),
+                                       (a[1], b[1]),
+                                       a_fx, a_fy,
+                                       (a[2], b[2]))
+        vx[grid] = vx[grid] + dt * a_fx / m[grid]
+        vy[grid] = vy[grid] + dt * a_fx / m[grid]
+        rx[grid] = rx[grid] + dt * vx[grid]
+        ry[grid] = ry[grid] + dt * vy[grid]
 
 def main(args):
     assert len(args) >= 1
